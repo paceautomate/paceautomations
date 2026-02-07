4fb2a32fbbceab04bfc7727769d114d14daeaedf
paceautomate <edwin@paceautomations.com>
2026-02-07 14:28:15 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import ScrollToTop from "@/components/ScrollToTop";
+
 import Index from "./pages/Index";
 import HowItWorks from "./pages/HowItWorks";
 import Solutions from "./pages/Solutions";
@@ -18,7 +21,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <BrowserRouter>
+        <ScrollToTop />
+
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/how-it-works" element={<HowItWorks />} />
@@ -26,7 +32,6 @@ const App = () => (
           <Route path="/security" element={<Security />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
