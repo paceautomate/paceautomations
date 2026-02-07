96718cba475852e5c399dd69f4cd8f6ab1175338
paceautomate <edwin@paceautomations.com>
2026-02-07 14:35:19 -0500

    Update ScrollToTop.tsx

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,16 +1,19 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export function ScrollToTop() {
+const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "instant", // no animation, feels native
+      behavior: "auto", // instant, native
     });
   }, [pathname]);
 
   return null;
-}
+};
+
+export default ScrollToTop;
+
