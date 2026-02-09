b8158335cbafb59f83f8512626d7ce198d249b2a
paceautomate <edwin@paceautomations.com>
2026-02-09 16:36:56 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/react";
+import { inject } from '@vercel/analytics';
 
 createRoot(document.getElementById("root")!).render(<App />);
